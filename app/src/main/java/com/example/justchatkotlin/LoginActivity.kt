618cb7b2fd1f6da39830d7abc09aa880955f0477
luis6156 <miculuis1@gmail.com>
2021-11-18 01:10:28 +0200

    added firebase auth & rounded corners

--- app/src/main/java/com/example/justchatkotlin/LoginActivity.kt
+++ app/src/main/java/com/example/justchatkotlin/LoginActivity.kt
@@ -3,21 +3,45 @@ package com.example.justchatkotlin
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        auth = Firebase.auth
+
         button_login.setOnClickListener {
-            val usernameLogin = username_login.text.toString()
+            val mailLogin = mail_login.text.toString()
             val passwordLogin = password_login.text.toString()
 
-            Log.d("LoginActivity", "email is $usernameLogin")
+            Log.d("LoginActivity", "email is $mailLogin")
             Log.d("LoginActivity", "name is $passwordLogin")
+
+            auth.signInWithEmailAndPassword(mailLogin, passwordLogin)
+                .addOnCompleteListener(this) {task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("Login", "signInWithEmail:success")
+                    val user = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("Login", "signInWithEmail:failure", task.exception)
+                }
+                }
+                .addOnFailureListener(this) {task ->
+                    Toast.makeText(baseContext, "${task.message}",
+                        Toast.LENGTH_SHORT).show()
+                }
         }
 
         need_an_account_textView.setOnClickListener {
--- app/src/main/java/com/example/justchatkotlin/MainActivity.kt
+++ app/src/main/java/com/example/justchatkotlin/MainActivity.kt
@@ -4,23 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_register.setOnClickListener {
-            val usernameRegister = username_register.text.toString()
-            val passwordRegister = password_register.text.toString()
-            val mailRegister = mail_register.text.toString()
+        auth = Firebase.auth
 
-            Log.d("MainActivity", "email is $mailRegister")
-            Log.d("MainActivity", "name is $usernameRegister")
-            Log.d("MainActivity", "password is $passwordRegister")
+        button_register.setOnClickListener {
+            performRegister()
         }
 
         already_have_account_textView.setOnClickListener {
@@ -29,5 +29,36 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun performRegister() {
+        val usernameRegister = username_register.text.toString()
+        val passwordRegister = password_register.text.toString()
+        val mailRegister = mail_register.text.toString()
+
+        Log.d("MainActivity", "email is $mailRegister")
+        Log.d("MainActivity", "name is $usernameRegister")
+        Log.d("MainActivity", "password is $passwordRegister")
+
+        if (mailRegister.isEmpty() || usernameRegister.isEmpty() || passwordRegister.isEmpty()) {
+            Toast.makeText(this, "Please enter data into all fields.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(mailRegister, passwordRegister)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("Main", "createUserWithEmail:success")
+                    val user = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("Main", "createUserWithEmail:failure", task.exception)
+                }
+            }
+            .addOnFailureListener(this) {task ->
+                Toast.makeText(baseContext, "${task.message}",
+                    Toast.LENGTH_SHORT).show()
+            }
     }
 }
